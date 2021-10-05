76b6226afd0a15a7a1c474044b014acfdb394e43
PineappleRind <pineapplerind.info@gmail.com>
2021-10-05 11:07:39 -0400

    1.1.1 =)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-document.body.insertAdjacentHTML('beforeend','<div id="BIGSURMODAL-CONTAINER"></div>')
+document.body.insertAdjacentHTML('beforeend', '<div id="BIGSURMODAL-CONTAINER"></div>')
 var BSM_modalShowing = false
 var BSM_queue = []
-function BigSurModal(obj) {
-    try{this.title = obj.title;if (!obj.title) throw new Error('BIG SUR MODAL: Modal needs a title.')} catch(e){return console.error(e)}
-    try{this.description = obj.description;if (!obj.description) throw new Error('BIG SUR MODAL: Modal needs a description.')} catch(e){return console.error(e)}
-    try{this.buttonContent = obj.buttons;if (!obj.buttons) throw new Error('BIG SUR MODAL: Modal needs buttons.')} catch(e){return console.error(e)}
 
+function BigSurModal(obj) {
+    try { this.title = obj.title; if (!obj.title) throw new Error('BIG SUR MODAL: Modal needs a title.') } catch (e) { return console.error(e) }
+    try { this.description = obj.description; if (!obj.description) throw new Error('BIG SUR MODAL: Modal needs a description.') } catch (e) { return console.error(e) }
+    try { this.buttonContent = obj.buttons; if (!obj.buttons) throw new Error('BIG SUR MODAL: Modal needs buttons.') } catch (e) { return console.error(e) }
     if (obj.theme) this.theme = obj.theme
     else {
         this.theme = 'BSM_theme_light'
@@ -13,11 +13,11 @@ function BigSurModal(obj) {
     }
     if (obj.appIcon) this.appIcon = obj.appIcon
     else {
-        this.appIcon = 'console_logo.png'
-        obj.appIcon = 'console_logo.png'
+        this.appIcon = 'https://pineapplerind.ga/libraries/bigsur-modal/console_logo.png'
+        obj.appIcon = 'https://pineapplerind.ga/libraries/bigsur-modal/console_logo.png'
     }
     this.show = function() {
-         this.createdModal = BSM_CreateModal(obj.title,obj.description,obj.buttons,obj.appIcon,obj.theme)
+        this.createdModal = BSM_CreateModal(obj)
     }
     this.close = function() {
         try {
@@ -29,72 +29,75 @@ function BigSurModal(obj) {
     }
 }
 var cont = document.getElementById('BIGSURMODAL-CONTAINER')
-function BSM_CreateModal(t,d,bc,ic,theme) {
+
+function BSM_CreateModal(obj) {
     let buttons = ``
 
-    for (let i = 0; i < bc.length; i++) {
-        if (bc.length >= 3) {
+    for (let i = 0; i < obj.buttons.length; i++) {
+        if (obj.buttons.length >= 3) {
             var vertical = 'vertical'
         } else {
             var vertical = ''
         }
-        if (!bc[i].default) bc[i].default = false
+        if (!obj.buttons[i].default) obj.buttons[i].default = false
         try {
-            if (!bc[i].name) throw new Error('BIG SUR MODAL: Button ' + i + ' needs a name.')
-            if (!bc[i].function) bc[i].function = 'BSM_DestroyModal()'
-            buttons += `<button onclick="${bc[i].function};BSM_DestroyModal()" class="BSM-btn ${bc[i].default}">${bc[i].name}</button>`
-        } catch(e) {
-            console.error(e)
+            if (!obj.buttons[i].name) throw new Error('bigsur-modal error: Button ' + i + ' needs a name.')
+            if (!obj.buttons[i].function) obj.buttons[i].function = ''
+            buttons += `<button onclick="${obj.buttons[i].function};BSM_DestroyModal()" class="BSM-btn ${obj.buttons[i].default}">${obj.buttons[i].name}</button>`
+        } catch (e) {
+            console.error('bigsur-modal internal error:' + e + '. Open an issue on the Github (https://github.com/pineapplerind/bigsur-modal)')
         }
     }
     var html = `
     <div class="BSM-appicon">
-        <img src="${ic}">
+        <img src="${obj.appIcon}">
     </div>
-    <h1 class="BSM-title">${t}</h1>
-    <h2 class="BSM-description">${d}</h1>
+    <h1 class="BSM-title">${obj.title}</h1>
+    <h2 class="BSM-description">${obj.description}</h1>
     <div class="BSM-btns ${vertical}">
         ${buttons}
     </div>
 
     `
     if (BSM_modalShowing == false) BSM_modalShowing = true
-    else if (BSM_modalShowing= true) return BSM_queue.push(html)
-    let modal = BSM_CreateModalHelper(html,theme)
+    else if (BSM_modalShowing = true) return BSM_queue.push(html)
+    let modal = BSM_CreateModalHelper(html, obj.theme)
     BSM_SetClose(modal)
     return modal
 }
-function BSM_CreateModalHelper(html,theme) {
+
+function BSM_CreateModalHelper(html, theme) {
     let BSM_modal = document.createElement('DIV')
     BSM_modal.innerHTML = html
     BSM_modal.classList.add('BSM-modal')
     BSM_modal.classList.add('BSM_theme_' + theme)
     cont.appendChild(BSM_modal)
     cont.classList.add('overlay')
-    console.log(BSM_modal)
     return BSM_modal
 }
-function BSM_SetClose(modal,defBtn) {
-      modal.onkeypress = e => {
-          if (e.keyCode == 13) defBtn.click()
-          else if (e.keyCode == 27) defBtn.click()
-      }
+
+function BSM_SetClose(md, db) {
+    md.onkeypress = e => {
+        if (e.keyCode == 13) db.click()
+        else if (e.keyCode == 27) db.click()
+    }
 }
+
 function BSM_DestroyModal() {
     let modal = cont.children[0]
-    console.log(modal)
     modal.style.animationName = 'comeout'
     BSM_modalShowing = false
     cont.classList.remove('overlay')
-    setTimeout(function(){
+    setTimeout(function() {
         modal.remove()
-    },80)
+    }, 80)
     if (BSM_queue.length == 0) return
     else {
         BSM_CreateModalFromQueue(BSM_queue[0])
     }
     console.log('Modal successfully closed')
 }
+
 function BSM_CreateModalFromQueue(html) {
     setTimeout(() => {
         BSM_CreateModalHelper(html)
